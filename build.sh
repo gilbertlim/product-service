66a7c94d2957332324092577fa4756214bf71546
gilbertlim <9ilbert.app@gmail.com>
2023-07-25 17:21:32 +0900

    fix msa app image

--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@
 
 REPOSITORY=9ilbert
 SERVICE=product
-TYPE=vanila
 VERSION=0.1
 
 ./gradlew build -x test
 docker run --privileged --rm tonistiigi/binfmt --install all
 docker buildx rm multiarch-builder
 docker buildx create --name multiarch-builder default --use
-docker buildx build --platform linux/amd64,linux/arm64 -t $REPOSITORY/$SERVICE:$TYPE-$VERSION . --push
+docker buildx build --platform linux/amd64 -t $REPOSITORY/$SERVICE:amd-$VERSION . --push
+docker buildx build --platform linux/arm64 -t $REPOSITORY/$SERVICE:arm-$VERSION . --push
+docker buildx build --platform linux/amd64,linux/arm64 -t $REPOSITORY/$SERVICE:multiarch-$VERSION . --push
